26efa5bb83d03be76a3131cc946cb89f646d9403
Olyveon <ferojasm@unal.edu.co>
2024-05-09 21:30:30 -0500

    added a test tilemap

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const camera_speed = 100.0
+const camera_speed = 200.0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
