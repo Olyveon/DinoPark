521b1b25eb27f8a5ac3952b27c6630a8702329cf
Olyveon <ferojasm@unal.edu.co>
2024-05-10 15:23:29 -0500

    added a building tilemap, a building mode with mouse click and a branch label in the UI

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends Node2D
 @onready var camera = $Camera2D
+@onready var tile_map = %TileMap
 
 const camera_speed = 200.0
 # Camera zoom values
@@ -14,7 +15,6 @@ func _process(delta):
 	var direction_y = Input.get_axis("move_up","move_down")
 	position.x += camera_speed * direction_x * delta
 	position.y += camera_speed * direction_y * delta
-
 # Checks if the mouse wheel it's changing and zooms in or out 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton:
@@ -27,3 +27,10 @@ func _input(event: InputEvent):
 				if camera.zoom < zoom_maximum:
 					camera.zoom += zoom_speed
 					pass
+			if event.button_index == MOUSE_BUTTON_LEFT:
+				var mouse_position = tile_map.local_to_map(Vector2i(get_global_mouse_position()))
+				print(mouse_position)
+				tile_map.set_cell(1,mouse_position,1,Vector2i(1,0))
+			if event.button_index == MOUSE_BUTTON_RIGHT:
+				var mouse_position = tile_map.local_to_map(Vector2i(get_global_mouse_position()))
+				tile_map.erase_cell(1,mouse_position)
